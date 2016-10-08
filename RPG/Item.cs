ea1c026206289cd8c46dfeb845d94a4cc837a620
GoodPie <Brandynbb96@gmail.com>
2016-10-08 18:22:02 +0800

    Added random name generator - Random name generator uses prefix and name file to generate random name - Created basic Item class

--- RPG/Item.cs
+++ RPG/Item.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RPG
+﻿namespace RPG
 {
     class Item
     {
@@ -55,6 +49,5 @@ namespace RPG
             get { return name; }
             set { name = value; }
         }
-
     }
 }
