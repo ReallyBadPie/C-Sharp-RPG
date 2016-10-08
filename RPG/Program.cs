ac96d8c0ee72655a9dfb74b7b913bd884386e4c3
Brandyn Britton <Goodpie@users.noreply.github.com>
2016-10-09 02:28:54 +0800

    Items (#3)
    * Created basic item
    
    * Added random name generator
    - Random name generator uses prefix and name file to generate random name
    - Created basic Item class

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RPG
 {
     class Program
     {
+        private List<Item> items;
+
         static void Main(string[] args)
         {
             bool gameRunning = true;
+
+            // Main game loop
             while(gameRunning)
             {
                 gameRunning = selectMenuOptions();
             }
         }
 
+        /**
+         * FUNCTION selectMenuOptions
+         * IMPORT None EXPORT None
+         * Shows the main menu options to the user
+         */
         private static bool selectMenuOptions()
         {
             // Default -1 for error check
@@ -59,15 +65,23 @@ namespace RPG
             return success;
         }
 
+        /**
+         * FUNCTION showMenu
+         * IMPORT None EXPORT None
+         * Shows the menu options to the user
+         */
         private static void showMenu()
         {
-            Console.WriteLine("Select Option: \n");
+            Console.WriteLine("===== [ MAIN MENU ] =====");
+
+            // All available current options
             Console.WriteLine("[1] Show Player Stats");
             Console.WriteLine("[2] Fight");
             Console.WriteLine("[3] Quest");
             Console.WriteLine("[4] Exit");
+
             Console.WriteLine("");
-            Console.WriteLine("Option: ");
+            Console.Write("Enter option: ");
         }
         
     }
