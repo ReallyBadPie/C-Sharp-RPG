431aea08c9009c3aadc61a008397231254ce323a
GoodPie <Brandynbb96@gmail.com>
2016-10-08 16:05:50 +0800

    Added main menu - Added basic placeholders for function calls - Added todo methods for future implementation - Added basic game control

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -10,9 +10,67 @@ namespace RPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("MEMEs");
-            Console.WriteLine("MEMEs");
-
+            bool gameRunning = true;
+            while(gameRunning)
+            {
+                gameRunning = selectMenuOptions();
+            }
         }
+
+        private static bool selectMenuOptions()
+        {
+            // Default -1 for error check
+            int selection = -1;
+            bool success = true;
+
+            // Display valid menu options
+            showMenu();
+
+            // Get user input and try convert to integer value
+            Int32.TryParse(Console.ReadLine(), out selection);
+
+            switch (selection)
+            {
+                case -1:
+                    Console.WriteLine("Error has occured getting user input");
+                    success = false;
+                    break;
+                case 1:
+                    Console.WriteLine("Showing player stats...");
+                    //TODO: show player stats
+                    break;
+                case 2:
+                    Console.WriteLine("Entering fight...");
+                    //TODO: Create fight options
+                    break;
+                case 3:
+                    Console.WriteLine("Enter quest mode...");
+                    //TODO: Create questing options
+                    break;
+                case 4:
+                    Console.WriteLine("Exiting Game...");
+                    success = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Try again!");
+                    break;
+            }
+
+            return success;
+        }
+
+        private static void showMenu()
+        {
+            Console.WriteLine("Select Option: \n");
+            Console.WriteLine("[1] Show Player Stats");
+            Console.WriteLine("[2] Fight");
+            Console.WriteLine("[3] Quest");
+            Console.WriteLine("[4] Exit");
+            Console.WriteLine("");
+            Console.WriteLine("Option: ");
+        }
+        
     }
+
+
 }
